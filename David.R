75466a87e9af860b64aff38940b675a40040e493
Czh3 <zhangchao3@hotmail.com>
2017-06-06 15:46:14 +0800

    hic

--- David.R
+++ David.R
@@ -230,7 +230,7 @@ Run_david_pathway <- function(species, geneID, ud) {
 		else if (species == "Mouse"){
 
 			if (geneID == "GENE_SYMBOL"){
-				library(org.Mm.eg.db, lib.loc = "~/tools/")
+				library(org.Mm.eg.db)
 				DEG <- select(org.Mm.eg.db, DEG, "ENTREZID", "SYMBOL")[,2]
 				geneID = "ENTREZ_GENE_ID"
 			}
